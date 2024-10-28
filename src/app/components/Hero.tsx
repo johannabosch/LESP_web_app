02b348d50340f08f541de8c1d3de1831be68fe25
johannabosch <yohannabosch@gmail.com>
2024-10-28 10:43:30 -0230

    updating hero

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -5,24 +5,24 @@ import React from 'react';
 const Hero: React.FC = () => {
   return (
     <div
-    className="relative h-[500px] bg-no-repeat bg-cover bg-right w-full mb-0"
+    className="relative h-[600px] bg-no-repeat bg-cover bg-right w-full mb-0"
     style={{
         backgroundImage: `url('/images/PIPL/Wildpix645.svg')`,
       }}
     >
       {/* Overlay content */}
-      <div className="absolute inset-0 flex flex-col z-10 p-8 mt-[80px]">
+      <div className="absolute inset-0 flex flex-col items-center right-[600px] z-10 p-8 mt-[80px]">
         {/* Main title */}
         <h1 className="text-4xl font-bold text-black drop-shadow-xl">
-          Demographic modeling for Atlantic Piping Plover 
+          Demographic modeling for Atlantic
         </h1>
         <h1 className="mb-[30px] text-4xl font-bold text-black drop-shadow-xl">
-        <i>(Charadrius melodus melodus)</i> in Eastern Canada
+        Piping Plover in Eastern Canada
         </h1>
 
         {/* Placeholder text */}
-        <div className="absolute top-[110px] transform w-[50vw] flex mb-5 text-lg text-black bg-white bg-opacity-60 rounded-[10px] py-[20px] px-[20px]">
-        <p> This web app offers an in-depth review of the Integrated Population Model (IPM) developed for the Atlantic Piping Plover in Eastern Canada. Researchers at Environment and Climate Change Canada have designed this model to integrate key monitoring data—abundance, productivity, and survival—into a cohesive framework that captures the population dynamics of this endangered species. Learn more about the history of Canada's vulnerable Piping Plover population below, or jump right in and <a href="/plover_page" className="text-blue-900 transition-all duration-200 hover:underline-offset-4 drop-shadow-xl font-semibold underline hover:underline">visualise the results of our model</a>. </p>
+        <div className="absolute top-[110px] transform w-[40vw] flex mb-5 text-lg text-black bg-white bg-opacity-60 rounded-[10px] py-[20px] px-[20px]">
+        <p> This web app offers an in-depth review of the Integrated Population Model (IPM) developed for the Atlantic Piping Plover <i>(Charadrius melodus melodus)</i> in Eastern Canada. We designed this model to _. Learn more about the history of Canada's Endangered Piping Plover population below, or jump right in and <a href="/plover_page" className="text-blue-900 transition-all duration-200 hover:underline-offset-4 drop-shadow-xl font-semibold underline hover:underline">visualise the results of our model</a>. </p>
         </div>
       </div>
     </div>
